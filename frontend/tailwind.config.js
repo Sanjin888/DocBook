f45ad48ce90938267a9d2e5b62320f34abfc8866
Sanjin888 <sanjinkurtalija@gmail.com>
2023-09-25 02:06:55 +0200

    hero section

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         primaryColor: "#0067FF",
         yellowColor : "#FEB60D",
         purpleColor : "#9771FF",
-        irisBlueColor : "01B5C5",
+        irisBlueColor : "#4DCBD6",
         headingColor: "#181A1E",
         textColor: "#4E545F",
       },
