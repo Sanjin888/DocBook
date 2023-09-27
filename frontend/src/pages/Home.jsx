02ac49c2e42d02eb166b2e06156635bfc99ba987
Sanjin888 <sanjinkurtalija@gmail.com>
2023-09-27 17:06:40 +0200

    services section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
@@ -6,7 +5,7 @@ import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 import { Link } from "react-router-dom";
-import { BsArrowRight} from "react-icons/bs" ;
+import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 
 const Home = () => {
@@ -78,6 +77,7 @@ const Home = () => {
         </div>
       </section>
       {/* ============ hero section =========*/}
+
       {/* =========== how it works section ========= */}
       <section>
         <div className="container">
@@ -106,7 +106,10 @@ const Home = () => {
                   unmatched, expert health care. From the lab to the clinic.
                 </p>
 
-                <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none " >
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none "
+                >
                   <BsArrowRight className="group-hover:text-white w-6 h-5 " />
                 </Link>
               </div>
@@ -125,7 +128,10 @@ const Home = () => {
                   unmatched, expert health care. From the lab to the clinic.
                 </p>
 
-                <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none " >
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none "
+                >
                   <BsArrowRight className="group-hover:text-white w-6 h-5 " />
                 </Link>
               </div>
@@ -144,7 +150,10 @@ const Home = () => {
                   unmatched, expert health care. From the lab to the clinic.
                 </p>
 
-                <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none " >
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none "
+                >
                   <BsArrowRight className="group-hover:text-white w-6 h-5 " />
                 </Link>
               </div>
@@ -154,8 +163,20 @@ const Home = () => {
       </section>
       {/* =========== how it works section end ========= */}
 
-      <About/>
+      <About />
       {/* ============== services section =========== */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto ">
+            <h2 className="heading text-center "> Our medical services</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* ============== services section end =========== */}
     </>
   );
 };
