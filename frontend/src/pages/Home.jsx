bace203900d1b1c0d6e9ba9aea2d1eb6ae6b1631
Sanjin888 <sanjinkurtalija@gmail.com>
2023-10-17 15:53:38 +0200

    testimonial component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,6 +14,7 @@ import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
 import FaqList from "../components/Faq/FaqList";
+import Testimonial from "../components/Testimonial/Testimonial";
 
 const Home = () => {
   return (
@@ -279,14 +280,33 @@ const Home = () => {
             </div>
 
             <div className="w-full md:w-1/2">
-              <h2 className="heading">Most questions asked by our beloved patients</h2>
+              <h2 className="heading">
+                Most questions asked by our beloved patients
+              </h2>
 
-              <FaqList/>
+              <FaqList />
             </div>
           </div>
         </div>
       </section>
       {/* ============== faq section end ============= */}
+
+      {/* ============== testimonial ============= */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto ">
+            <h2 className="heading text-center "> What our patients say</h2>
+            <p className="text__para teqxt-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+
+          <Testimonial/>
+        </div>
+      </section>
+
+      {/* ============== testimonial end ============= */}
     </>
   );
 };
