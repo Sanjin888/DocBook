784e4c4b50c769f0bebe38583b13f1de7ffa3832
Sanjin888 <sanjinkurtalija@gmail.com>
2023-10-27 01:29:28 +0200

    login and signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import signupImg from "../assets/images/signup.gif";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <section className="px-5 xl:px-0">
+      <div className="mx-w-[1170px] mx-auto ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 ">
+          {/*======== img box ========= */}
+          <div className=" hidden lg:block bg-primaryColor rounded-l-lg ">
+            <figure className="rounded-l-lg">
+              <img src={signupImg} alt="" className="w-full rounded-l-lg" />
+            </figure>
+          </div>
+
+          {/* ======== sign up form =======*/}
+          <div className="rounded">
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
