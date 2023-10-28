73894212971ba9f670c5db40075d0843c7bd4aa8
Sanjin888 <sanjinkurtalija@gmail.com>
2023-10-29 00:57:59 +0200

    user and doctor routes

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -45,7 +45,7 @@ export const getSingleUser = async (req, res) => {
   const id = req.params.id;
 
   try {
-    const user = await User.findById(id);
+    const user = await User.findById(id).select("-password");
 
     res.status(200).json({
       success: true,
@@ -62,7 +62,7 @@ export const getSingleUser = async (req, res) => {
 
 export const getAllUser = async (req, res) => {
   try {
-    const users = await User.find({});
+    const users = await User.find({}).select("-password");
 
     res.status(200).json({
       success: true,
