03446d29324b61ecf3db72b93a8a8d8ebd0dcb8a
Sanjin888 <sanjinkurtalija@gmail.com>
2023-12-07 22:56:20 +0100

    reviews backend

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -14,9 +14,9 @@ const router = express.Router()
 //nested route
 router.use("/:doctorId/reviews", reviewRouter);
 
-router.get("/:id", getSingleDoctor)
-router.get("/", getAllDoctor)
-router.put("/:id",authenticate, restrict(["doctor"]), updateDoctor)
-router.delete("/:id",authenticate, restrict(["doctor"]), deleteDoctor)
+router.get("/:id", getSingleDoctor);
+router.get("/", getAllDoctor);
+router.put("/:id",authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id",authenticate, restrict(["doctor"]), deleteDoctor);
 
 export default router;
